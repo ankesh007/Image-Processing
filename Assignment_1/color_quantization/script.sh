8f03f16fc1a00c78cb498cb07c0baf078805d19a
ankesh007 <ankeshgupta007@gmail.com>
2018-08-22 16:53:04 +0530

    Added more results

--- Assignment_1/color_quantization/script.sh
+++ Assignment_1/color_quantization/script.sh
@@ -15,3 +15,12 @@ python median_cut.py ../Images/low_qual_400/Mario.jpg ../Result/Mario_MD2.jpg 2 
 python median_cut.py ../Images/low_qual_400/Mario.jpg ../Result/Mario_MD5.jpg 5 True
 python median_cut.py ../Images/low_qual_400/Mario.jpg ../Result/Mario_MD10.jpg 10 True
 python median_cut.py ../Images/low_qual_400/Mario.jpg ../Result/Mario_MD20.jpg 20 True
+
+python median_cut.py ../Images/low_qual_400/Smile.jpg ../Result/Smile_M2.jpg 2 False
+python median_cut.py ../Images/low_qual_400/Smile.jpg ../Result/Smile_M5.jpg 5 False
+python median_cut.py ../Images/low_qual_400/Smile.jpg ../Result/Smile_M10.jpg 10 False
+python median_cut.py ../Images/low_qual_400/Smile.jpg ../Result/Smile_M20.jpg 20 False
+python median_cut.py ../Images/low_qual_400/Smile.jpg ../Result/Smile_MD2.jpg 2 True
+python median_cut.py ../Images/low_qual_400/Smile.jpg ../Result/Smile_MD5.jpg 5 True
+python median_cut.py ../Images/low_qual_400/Smile.jpg ../Result/Smile_MD10.jpg 10 True
+python median_cut.py ../Images/low_qual_400/Smile.jpg ../Result/Smile_MD20.jpg 20 True
--- Assignment_1/color_quantization/script_popularity.sh
+++ Assignment_1/color_quantization/script_popularity.sh
@@ -15,3 +15,12 @@ python popularity.py ../Images/low_qual_400/Mario.jpg ../Result/Mario_PD2.jpg 2 
 python popularity.py ../Images/low_qual_400/Mario.jpg ../Result/Mario_PD5.jpg 5 True
 python popularity.py ../Images/low_qual_400/Mario.jpg ../Result/Mario_PD10.jpg 10 True
 python popularity.py ../Images/low_qual_400/Mario.jpg ../Result/Mario_PD20.jpg 20 True
+
+python popularity.py ../Images/low_qual_400/Smile.jpg ../Result/Smile_P2.jpg 2 False
+python popularity.py ../Images/low_qual_400/Smile.jpg ../Result/Smile_P5.jpg 5 False
+python popularity.py ../Images/low_qual_400/Smile.jpg ../Result/Smile_P10.jpg 10 False
+python popularity.py ../Images/low_qual_400/Smile.jpg ../Result/Smile_P20.jpg 20 False
+python popularity.py ../Images/low_qual_400/Smile.jpg ../Result/Smile_PD2.jpg 2 True
+python popularity.py ../Images/low_qual_400/Smile.jpg ../Result/Smile_PD5.jpg 5 True
+python popularity.py ../Images/low_qual_400/Smile.jpg ../Result/Smile_PD10.jpg 10 True
+python popularity.py ../Images/low_qual_400/Smile.jpg ../Result/Smile_PD20.jpg 20 True
